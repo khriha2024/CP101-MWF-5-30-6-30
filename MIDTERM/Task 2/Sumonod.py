f1faaccce592b941341b38e5f3a13304d2159f69
khriha2024 <khrishasomunod@gmail.com>
2024-09-26 12:34:46 +0800

    Sumonod.py

---
+++ MIDTERM/Task 2/Sumonod.py
@@ -0,0 +1,8 @@
+"Write  Phython Program to Print All Numbers Between 1 to 100 Using While loop"
+print("Even Numbers Between 1 to 100...")
+
+index=1
+while index < 100:
+    if index % 2 == 0:
+        print(index, end=" ")
+    index += 1    
