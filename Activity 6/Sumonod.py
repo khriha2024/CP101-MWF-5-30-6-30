b6b1a760e9312bbd8308b967769db9e8f42680f5
khriha2024 <khrishasomunod@gmail.com>
2024-09-30 00:19:21 +0800

    Sumonod.py

---
+++ Activity 6/Sumonod.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(f"Hello, {name}! You are {age} years old.")
+
+
+item = "orange"
+count = 10
+sentence = "I bought {} {} today.".format(count, item)
+print(sentence)
+
+
+city = "Dipolog Ciity"
+temperature =27
+print("The temperature in %s is %d degrees Celsius." % (city, temperature))
